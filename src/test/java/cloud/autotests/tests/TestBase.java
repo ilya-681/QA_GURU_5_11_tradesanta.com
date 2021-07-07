772d1c24724c2bf093a83c91f56d28df66eb6d08
ilya <ilya-681@ya.ru>
2021-07-08 02:43:15 +0300

    Changed close webdriver order

--- src/test/java/cloud/autotests/tests/TestBase.java
+++ src/test/java/cloud/autotests/tests/TestBase.java
@@ -34,10 +34,9 @@ public class TestBase {
         AllureAttachments.addPageSource();
         AllureAttachments.addBrowserConsoleLogs();
 
-        Selenide.closeWebDriver();
-
         if (Project.isVideoOn()) {
             AllureAttachments.addVideo(sessionId);
         }
+        Selenide.closeWebDriver();
     }
 }
